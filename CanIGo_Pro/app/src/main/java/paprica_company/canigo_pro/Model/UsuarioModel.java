9f613e55ac35b0efe518f7b85494efd9519b0e09
cmedeirosviana <caiomedeiros4@hotmail.com>
2016-01-14 20:41:22 -0200

    Login Screen
    Login Screen para avaliação

---
+++ CanIGo_Pro/app/src/main/java/paprica_company/canigo_pro/Model/UsuarioModel.java
@@ -0,0 +1,23 @@
+package paprica_company.canigo_pro.Model;
+
+
+public class UsuarioModel
+{
+    String login;
+    String senha;
+    String email;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+}
