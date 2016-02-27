b67d27ffe991d99ba96940f93b79cf187383d3ca
luanacm <luanamontandon@yahoo.com.br>
2016-02-27 16:18:35 -0300

    Pin Update

--- CanIGo_Pro/app/src/main/java/paprica_company/canigo_pro/Model/User.java
+++ CanIGo_Pro/app/src/main/java/paprica_company/canigo_pro/Model/User.java
@@ -7,10 +7,8 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import paprica_company.canigo_pro.Dao.DBAdapter;
 import paprica_company.canigo_pro.Dao.Script_User;
-
 
 public class User
 {
@@ -38,30 +36,35 @@ public class User
 
     private final Context mCtx;
 
-    private static class DatabaseHelper extends SQLiteOpenHelper {
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
 
-        DatabaseHelper(Context context) {
+        DatabaseHelper(Context context)
+        {
             super(context, DBAdapter.DATABASE_NAME, null, DBAdapter.DATABASE_VERSION);
         }
 
         @Override
-        public void onCreate(SQLiteDatabase db) {
+        public void onCreate(SQLiteDatabase db)
+        {
         }
 
         @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
         }
     }
     //-------------------------------------- Builders----------------------------------------------
-    public User(String name, String email, String year) {
-
+    public User(String name, String email, String year)
+    {
         this.mCtx = DBAdapter.getContext();
         open();
         createUser(name, email, year);
         close();
     }
     //---------------------------------- Table DB Methods------------------------------------------
-    public User open() throws SQLException {
+    public User open() throws SQLException
+    {
         this.mDbHelper = new DatabaseHelper(this.mCtx);
         this.mDb = this.mDbHelper.getWritableDatabase();
         return this;
@@ -71,14 +74,14 @@ public class User
         this.mDbHelper.close();
     }
 
-    private long createUser(String name, String email, String year){
+    private long createUser(String name, String email, String year)
+    {
         ContentValues initialValues = new ContentValues();
         initialValues.put(Script_User.FeedEntry.COLUMN_LOGIN, name);
         initialValues.put(Script_User.FeedEntry.COLUMN_EMAIL, email);
         initialValues.put(Script_User.FeedEntry.COLUMN_PWD, year);
         return this.mDb.insert(Script_User.FeedEntry.TABLE_NAME, null, initialValues);
     }
-
 
     public boolean deleteUser(long rowId) {
 
