354f6ea51a5065a5c12eee9d6144e6a1eb5ff621
luanacm <luanamontandon@yahoo.com.br>
2016-03-26 11:55:32 -0300

    Navigation View Creation

--- CanIGo_Pro/app/src/main/java/paprica_company/canigo_pro/View/MainActivity.java
+++ CanIGo_Pro/app/src/main/java/paprica_company/canigo_pro/View/MainActivity.java
@@ -1,6 +1,7 @@
 package paprica_company.canigo_pro.View;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -18,9 +18,7 @@ import paprica_company.canigo_pro.R;
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener,MenuItem.OnMenuItemClickListener
 {
 
-    private String[] mPlanetTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private DrawerLayout drawerLayout ;
 
 
     @Override
@@ -70,35 +68,33 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
             }
         });
 
-        versusBtn.setOnClickListener(new View.OnClickListener()
-        {
+        versusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 //TODO : falta criar a conexão com os outros modulos
             }
         });
 
-        perfilBtn.setOnClickListener(new View.OnClickListener()
-        {
+        perfilBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 //TODO : falta criar a conexão com os outros modulos
             }
         });
 
         //-------------------------------------- Navigation Implementation --------------------------------------------
 
-        mPlanetTitles = getResources().getStringArray(R.array.nome);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.navigation_list_item, mPlanetTitles));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
     }
     //-------------------------------------- Menu Implementation --------------------------------------------
 
